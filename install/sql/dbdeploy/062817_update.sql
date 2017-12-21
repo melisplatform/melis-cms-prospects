f678e941865076d0e4859c716dca5bb60e7078c8
Ian Borla <benborla@icloud.com>
2017-12-21 11:27:46 +0800

    dbdeploy update test

--- install/sql/dbdeploy/062817_update.sql
+++ install/sql/dbdeploy/062817_update.sql
@@ -5,7 +5,7 @@ CHANGE `pros_site_id` `pros_site_id` INT(11) NULL,
 CHANGE `pros_type` `pros_type` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL, 
 CHANGE `pros_theme` `pros_theme` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL, 
 CHANGE `pros_name` `pros_name` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL, 
-CHANGE `pros_email` `pros_email` VARCHAR(150) CHARACTER SET utf8 COLLATE utf8_general_ci NULL, 
+CHANGE `pros_email` `pros_email` VARCHAR(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
 CHANGE `pros_telephone` `pros_telephone` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL, 
 CHANGE `pros_societe` `pros_societe` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL, 
 CHANGE `pros_message` `pros_message` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
