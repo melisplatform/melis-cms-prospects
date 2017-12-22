40ae1f5bca081826bbd045051f086ff19019088b
Ian Borla <benborla@icloud.com>
2017-12-22 17:12:07 +0800

    update sql

--- install/sql/dbdeploy/062817_update.sql
+++ install/sql/dbdeploy/062817_update.sql
@@ -6,10 +6,6 @@ CHANGE `pros_type` `pros_type` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_gene
 CHANGE `pros_theme` `pros_theme` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL, 
 CHANGE `pros_name` `pros_name` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL, 
 CHANGE `pros_email` `pros_email` VARCHAR(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-CHANGE `pros_telephone` `pros_telephone` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL, 
-CHANGE `pros_societe` `pros_societe` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL, 
+CHANGE `pros_telephone` `pros_telephone` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
 CHANGE `pros_message` `pros_message` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
 
-ALTER TABLE `melis_cms_prospects` 
-ADD `pros_company` VARCHAR(45) NULL AFTER `pros_message`, 
-ADD `pros_country` VARCHAR(45) NULL AFTER `pros_company`;
