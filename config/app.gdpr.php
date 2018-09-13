db85884e70e9d6980378c3f7458b9a3dacad7817
Franz Joseph Paran <fparan@melistechnology.com>
2018-09-13 14:08:12 +0800

    Added style and class on config

--- config/app.gdpr.php
+++ config/app.gdpr.php
@@ -10,23 +10,38 @@
                             'columns' => [
                                 'pros_email' => [
                                     'id' => 'meliscmsprospects_pros_email',
-                                    'text' => 'tr_melis_cms_prospects_gdpr_column_email'
+                                    'class' => '',
+                                    'style' => 'width:1%;padding-left:65px !important;',
+                                    'text' => 'tr_melis_cms_prospects_gdpr_column_email',
+                                    'sorting' => false
                                 ],
                                 'pros_name' => [
                                     'id' => 'meliscmsprospects_pros_name',
-                                    'text' => 'tr_melis_cms_prospects_gdpr_column_name'
+                                    'class' => '',
+                                    'style' => 'padding-left:70px !important;',
+                                    'text' => 'tr_melis_cms_prospects_gdpr_column_name',
+                                    'sorting' => false
                                 ],
                                 'pros_company' => [
                                     'id' => 'meliscmsprospects_pros_company',
-                                    'text' => 'tr_melis_cms_prospects_gdpr_column_company'
+                                    'class' => '',
+                                    'style' => '',
+                                    'text' => 'tr_melis_cms_prospects_gdpr_column_company',
+                                    'sorting' => false
                                 ],
                                 'pros_country' => [
                                     'id' => 'meliscmsprospects_pros_country',
-                                    'text' => 'tr_melis_cms_prospects_gdpr_column_country'
+                                    'class' => '',
+                                    'style' => '',
+                                    'text' => 'tr_melis_cms_prospects_gdpr_column_country',
+                                    'sorting' => false
                                 ],
                                 'site_name' => [
                                     'id' => 'meliscmsprospects_pros_site',
-                                    'text' => 'tr_melis_cms_prospects_gdpr_column_site'
+                                    'class' => '',
+                                    'style' => '',
+                                    'text' => 'tr_melis_cms_prospects_gdpr_column_site',
+                                    'sorting' => false
                                 ],
                             ],
                         ],
