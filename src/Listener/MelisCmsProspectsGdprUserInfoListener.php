4e712f07caa4e34cd69739c5589a6be1814209d2
fparan <fparan@melistechnology.com>
2019-05-20 17:28:43 +0800

    GDPR Update - Specific or nonspecific name search

--- src/Listener/MelisCmsProspectsGdprUserInfoListener.php
+++ src/Listener/MelisCmsProspectsGdprUserInfoListener.php
@@ -16,6 +16,8 @@ use Zend\ServiceManager\ServiceManager;
 
 class MelisCmsProspectsGdprUserInfoListener extends MelisCoreGeneralListener implements ListenerAggregateInterface
 {
+    protected $sl;
+
     public function attach(EventManagerInterface $events)
     {
         $sharedEvents = $events->getSharedManager();
@@ -27,26 +29,18 @@ class MelisCmsProspectsGdprUserInfoListener extends MelisCoreGeneralListener imp
             ],
             function($e)
             {
-                $moduleName = $this->getModuleName($this);
-                $prospectsTable = $e->getTarget()->getServiceLocator()->get('MelisProspects');
-                $melisCoreConfig = $e->getTarget()->getServiceLocator()->get('config');
                 $parameters = $e->getParams();
-
-                $dataConfig = $melisCoreConfig['plugins'][$moduleName]['gdpr']['getUserInfo'];
-
-                //get all keys of columns
+                $moduleName = $this->getModuleName();
+                $this->sl = $e->getTarget()->getServiceLocator();
+                $dataConfig = $this->getConfig($moduleName);
                 $tableColumns = array_keys($dataConfig['values']['columns']);
+                $specificSearch = isset($parameters['search']['specific-search']) ? true : false;
+                $arrayDatas = $this->getService('MelisProspects')->getDataForGdpr($parameters['search'], $specificSearch)->toArray();
 
-                $searchableColumns = [
-                    'user_name' => 'pros_name',
-                    'user_email' => 'pros_email',
-                    'site_id' => 'pros_site_id'
-                ];
-
-                $arrayDatas = $prospectsTable->getDataForGdpr($parameters['search'], $searchableColumns)->toArray();
                 //module should stay silent if no data mataches
                 if (!empty($arrayDatas)) {
                     $dataConfig['values']['datas'] = $this->structureDatasArray($arrayDatas, $tableColumns, $dataConfig);
+                    //send data back
                     $parameters['results'][$moduleName] = $dataConfig;
                 }
             });
@@ -58,7 +52,7 @@ class MelisCmsProspectsGdprUserInfoListener extends MelisCoreGeneralListener imp
      * @param Class
      * @return String = module name
      */
-    private function getModuleName($class)
+    private function getModuleName()
     {
         $controllerClass = get_class($this);
         $moduleName = substr($controllerClass, 0, strpos($controllerClass, '\\'));
@@ -87,5 +81,26 @@ class MelisCmsProspectsGdprUserInfoListener extends MelisCoreGeneralListener imp
         }
 
         return $datasArray;
+    }
+
+    /**
+     * Returns Service
+     * @param $service
+     * @return mixed
+     */
+    private function getService($service)
+    {
+        return $this->sl->get($service);
+    }
+
+    /**
+     * Returns gdpr config
+     * @param $moduleName
+     * @return mixed
+     */
+    private function getConfig($moduleName)
+    {
+        $config = $this->getService('config');
+        return $config['plugins'][$moduleName]['gdpr']['getUserInfo'];
     }
 }
